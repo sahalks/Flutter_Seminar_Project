84c2d7a9285c4a1884aef1cb852880bdb2a4ebb5
Sahal <sahalsaidu123@gmail.com>
2022-12-01 12:04:29 +0530

    Project

---
+++ lib/Lazy Loading Seminar/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:seminar_project/Lazy%20Loading%20Seminar/Lazy_Loading.dart';
+
+void main(){
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: LazyLoading(),
+  ));
+}
